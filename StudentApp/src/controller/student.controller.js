1c47aee4eeb6391b23b6f927c94c6201440135d5
pranay-panchal <pranayy0208@gmail.com>
2019-06-16 16:55:52 +0530

    put and el api remaining

--- StudentApp/src/controller/student.controller.js
+++ StudentApp/src/controller/student.controller.js
@@ -1,12 +1,53 @@
+const StudentBiz = require('../biz/student.biz');
+const ResponseModel = require('../models/responseModel.models');
+
 module.exports = {
     getStudentInfo: async (request, response) => {
         try {
-            const id = request.params.id;
-            console.log(id);
-            //call biz func here
-            response.status(400).json({
-                sucess: true
+            const studentBiz = new StudentBiz();
+            const responseModel = new ResponseModel();
+            const data = await studentBiz.getStudent();
+            if (data.length < 0) {
+                responseModel.sucess = false;
+                responseModel.data = 'OOps something went wrong..'
+            } 
+            responseModel.sucess = true;
+            responseModel.data = data;
+            response.status(200).json(responseModel);
+        } catch (error) {
+            response.status(500).json({
+                sucess: false,
+                error: error.message
+            });
+        }
+    },
+
+    addStudentInfo: async(request, response) => {
+        try {
+            const data = request.body;
+            const studentBiz = new StudentBiz();
+            await studentBiz.addStudent(data);
+            response.status(200).json({
+                sucess: true,
+                record_added : true
             })
+        } catch (error) {
+            response.status(500).json({
+                sucess: false,
+                error: error.message
+            });
+        }
+    },
+
+    getStudentById: async(request, response) => {
+        try {
+            const id = request.params.id;
+            const studentBiz = new StudentBiz();
+            const responseModel = new ResponseModel();
+            const data = await studentBiz.getStudentById(id);
+            responseModel.sucess = true;
+            responseModel.data = data;
+            response.status(200).json(responseModel);
         } catch (error) {
             response.status(500).json({
                 sucess: false,
---
+++ StudentApp/src/models/responseModel.models.js
@@ -0,0 +1,8 @@
+class ResponseModel {
+    constructor(sucess,data) {
+        this.sucess = sucess;
+        this.data = data;
+    }
+} 
+
+module.exports = ResponseModel;
