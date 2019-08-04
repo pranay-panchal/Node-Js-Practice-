73ef696c63c0987db5140862942093de0442ba9a
pranay-panchal <pranayy0208@gmail.com>
2019-08-04 12:20:24 +0530

    routes changed to api

--- StudentApp/app.js
+++ StudentApp/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const studentRoutes = require('./src/routes/student');
+const studentRoutes = require('./src/api/student');
 const path = require('path');
 
 app.use(express.text());
