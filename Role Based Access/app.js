12ae76e7efaec3248c9cff260aaaa0bc0ef85f10
pranay-panchal <pranayy0208@gmail.com>
2019-11-03 11:04:07 +0530

    added database.js in cofig

--- Role Based Access/app.js
+++ Role Based Access/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+
 const app = express();
 
+// parsing requests
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded());
 
-const PORT = 5000;
+// defining the middlewares
+app.use(cors());
 
-app.listen();
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: "This is nodejs Role based authentication system"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port... ${PORT}`);
+});
---
+++ Role Based Access/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongo://localhost:27017/authentication_app',
+    secret: 'hello'
+}
